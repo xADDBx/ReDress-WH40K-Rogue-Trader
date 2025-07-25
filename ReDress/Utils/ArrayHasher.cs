625f8f4f85a2e0c3818895723961357a8f3637c9
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-07-25 15:36:04 +0200

    Remove accidental leftover

--- ReDress/Utils/ArrayHasher.cs
+++ ReDress/Utils/ArrayHasher.cs
@@ -6,7 +6,6 @@ public static class ArrayHasher {
     private static readonly XxHash64 m_Hasher = new();
     public static ulong ComputeHash(List<EntityPartStorage.CustomColor> data, int height, int width) {
         var floats = new float[3 * height * width];
-        data.SelectMany<EntityPartStorage.CustomColor, float>(c => [c.R, c.G, c.B]).ToArray().AsSpan();
         for (int i = 0; i < data.Count; i++) {
             var tmp = data[i];
             floats[3 * i] = tmp.R;
