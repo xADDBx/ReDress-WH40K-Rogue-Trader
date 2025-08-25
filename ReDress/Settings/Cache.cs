ccab857b37b72e92ddd4e283b912c02c74482f4e
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-08-25 17:05:57 +0200

    Add custom EE support by scanning Owlcat Mods (and shipped assets) This will make another (a lot faster) cache rebuild when the OMM mods changed; which doesn't touch the cache that's created when game version changes.

--- ReDress/Settings/Cache.cs
+++ ReDress/Settings/Cache.cs
@@ -3,7 +3,9 @@ using Kingmaker;
 using Kingmaker.Blueprints;
 using Kingmaker.BundlesLoading;
 using Kingmaker.GameInfo;
+using Kingmaker.Modding;
 using Kingmaker.Visual.CharacterSystem;
+using Newtonsoft.Json;
 
 namespace ReDress; 
 internal class Cache : AbstractSettings {
@@ -11,7 +13,26 @@ internal class Cache : AbstractSettings {
     public bool ToggleSearchAsYouType = true;
     public float SearchDelay = 0.3f;
     public string CachedVersion = "";
-    public Dictionary<string, string> AssetMapping = [];
+    public Dictionary<string, string> BaseGameEEs = [];
+    public Dictionary<string, string> ModdedEEs = [];
+    [JsonIgnore]
+    public Dictionary<string, string>? AssetMapping {
+        get {
+            if (field == null) {
+                field = new(BaseGameEEs);
+                foreach (var item in ModdedEEs) {
+                    if (!field.ContainsKey(item.Key)) {
+                        field[item.Key] = item.Value;
+                    } else {
+                        Main.Log.Error($"Id collision for modded EE: {item.Key}");
+                    }
+                }
+            }
+            return field;
+        }
+        private set;
+    }
+    public HashSet<(string, string)> OmmSet = [];
     public bool ShouldExcludeNewEEs = false;
     protected override string Name => "Settings.json";
     private static readonly Lazy<Cache> _instance = new Lazy<Cache>(() => {
@@ -20,27 +41,68 @@ internal class Cache : AbstractSettings {
         return instance;
     });
     public static Cache CacheInstance => _instance.Value;
-    public static bool NeedsCacheRebuilt => (GameVersion.GetVersion() != CacheInstance.CachedVersion) && !m_IsRebuilding && CacheInstance.AssetMapping?.Count <= 0;
+    private static bool m_BaseGameRebuild = false;
+    public static bool? NeedsCacheRebuilt {
+        get {
+            if (CacheInstance.AssetMapping!.Count < 1) {
+                m_BaseGameRebuild = true;
+                return true;
+            }
+            if (field.HasValue) {
+                return field.Value && !m_IsRebuilding;
+            }
+
+            bool gameVersionChanged = GameVersion.GetVersion() != CacheInstance.CachedVersion;
+            field = gameVersionChanged;
+            m_BaseGameRebuild = gameVersionChanged;
+            Main.Log.Log($"Test for ReDress Cache consistency: Did Game Version change?: {gameVersionChanged}");
+
+            bool ommModsChanged = !(CacheInstance.OmmSet.Count == OwlcatModificationsManager.s_Instance.AppliedModifications.Length);
+            if (!ommModsChanged) {
+                foreach (var modEntry in OwlcatModificationsManager.s_Instance.AppliedModifications) {
+                    if (!CacheInstance.OmmSet.Contains(new(modEntry.Manifest.UniqueName, modEntry.Manifest.Version))) {
+                        ommModsChanged = true;
+                        break;
+                    }
+                }
+            }
+            field |= ommModsChanged;
+            Main.Log.Log($"^- Test for ReDress Cache consistency: Did Owlmods change?: {ommModsChanged}");
+            return field.Value && !m_IsRebuilding;
+        }
+    }
     private static bool m_IsRebuilding = false;
     internal static void RebuildCache() {
         m_IsRebuilding = true;
-        CacheInstance.AssetMapping.Clear();
-        foreach (var guid in BundlesLoadService.Instance.m_LocationList.Guids.Where(g => BundlesLoadService.Instance.m_LocationList.GuidToBundle[g] == "equipment")) {
+        if (m_BaseGameRebuild) {
+            CacheInstance.BaseGameEEs.Clear();
+            foreach (var guid in BundlesLoadService.Instance.m_LocationList.Guids.Where(g => BundlesLoadService.Instance.m_LocationList.GuidToBundle[g] == "equipment")) {
+                var obj = ResourcesLibrary.TryGetResource<UnityEngine.Object>(guid, true, false);
+                if (obj is EquipmentEntity ee) {
+                    CacheInstance.BaseGameEEs[guid] = ee.name;
+                }
+            }
+        }
+        CacheInstance.ModdedEEs.Clear();
+        foreach (var guid in OwlcatModificationsManager.Instance.AppliedModifications.SelectMany(mod => mod.Settings.BundlesLayout.Guids)) {
             var obj = ResourcesLibrary.TryGetResource<UnityEngine.Object>(guid, true, false);
             if (obj is EquipmentEntity ee) {
-                CacheInstance.AssetMapping[guid] = ee.name;
+                CacheInstance.ModdedEEs[guid] = ee.name;
             }
         }
         ResourcesLibrary.CleanupLoadedCache(ResourcesLibrary.CleanupMode.UnloadNonRequested);
         CacheInstance.CachedVersion = GameVersion.GetVersion();
+        CacheInstance.OmmSet = [.. OwlcatModificationsManager.s_Instance.AppliedModifications.Select<OwlcatModification, (string, string)>(m => new(m.Manifest.UniqueName, m.Manifest.Version))];
+        CacheInstance.AssetMapping = null;
         CacheInstance.Save();
+        m_BaseGameRebuild = false;
         m_IsRebuilding = false;
     }
     [HarmonyPatch(typeof(GameMainMenu))]
     public static class RebuildCachePatch {
         [HarmonyPatch(nameof(GameMainMenu.Start)), HarmonyPostfix]
         public static void Start() {
-            if (NeedsCacheRebuilt) {
+            if (NeedsCacheRebuilt.HasValue && NeedsCacheRebuilt.Value) {
                 RebuildCache();
             }
         }
